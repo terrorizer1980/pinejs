2d0711614958ce64dc5371eeed3f26923f0712de
Pagan Gazzard <page@resin.io>
2018-08-29 08:49:32 +0100

    More typing improvements
    Change-type: patch
    Signed-off-by: Pagan Gazzard <page@resin.io>

--- src/sbvr-api/permissions.d.ts
+++ src/sbvr-api/permissions.d.ts
@@ -2,8 +2,8 @@ import * as Promise from 'bluebird'
 import * as sbvrUtils from './sbvr-utils'
 import * as express from 'express'
 
-export const root: sbvrUtils.User
-export const rootRead: sbvrUtils.User
+export const root: { user: sbvrUtils.User }
+export const rootRead: { user: sbvrUtils.User }
 
 export function getUserPermissions(userId: number, callback?: (err: Error | undefined, permissions: string[]) => void): Promise<string[]>;
 export function apiKeyMiddleware(req: express.Request, res?: express.Response, next?: express.NextFunction): Promise<void>;
